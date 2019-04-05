e9a86ff737f796730d645bd2cb7fd26f5ddb646d
Joseph Antaki <antakij1@students.rowan.edu>
2019-04-05 14:49:46 -0400

    reorganize

--- preliminary.py
+++ preliminary.py
@@ -1,8 +1,7 @@
 import nltk
 import csv
 import pickle
-
-curses = ['fuck', 'fucking', 'fucked', 'fucks', 'shit', 'shits', 'damn', 'damned']
+curses = ['fuck', 'fucking', 'fucked', 'fucks', 'shit', 'shits', 'damn', 'damned', 'crap']
 
 def count_screaming(words):
     score = 0
@@ -18,27 +17,33 @@ def count_cursing(words):
             score += 1
     return score
 
+def avg_wordlength(words):
+    numerator = 0.0
+    denominator = len(words)
+    for w in words:
+        numerator = numerator + len(w)
+    return numerator/denominator if denominator != 0 else 0
+
 def generate_array(path):
     results = []
     with open(path, newline='', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter=',')
         for row in reader:
             tokenized = nltk.word_tokenize(row[3])
-            results.append([count_screaming(tokenized), count_cursing(tokenized)])
+            tokenized = [w for w in tokenized if not w in stop_words]
+            results.append([count_screaming(tokenized), count_cursing(tokenized), avg_wordlength(tokenized)])
     return results
 
-
+stop_words = set(nltk.corpus.stopwords.words('english'))
 ###change these paths to be specific to your computer###
-train_array = generate_array("C:/Users/Joe/Downloads/fake-news/train_clean.csv")
-test_array = generate_array("C:/Users/Joe/Downloads/fake-news/test_clean.csv")
+train_array = generate_array("C:/Users/Joe/PycharmProjects/fakenews/data/train_clean.csv")
 
 #This is done outside of generate_array because the test set does not have labels to check against
 labels = []
-with open("C:/Users/Joe/Downloads/fake-news/train_clean.csv", newline='', encoding='utf-8') as file:
+with open("C:/Users/Joe/PycharmProjects/fakenews/data/train_clean.csv", newline='', encoding='utf-8') as file:
     reader = csv.reader(file, delimiter=',')
     for row in reader:
         labels.append(row[4])
 
-pickle.dump(train_array, open("C:/Users/Joe/Downloads/fake-news/train_array.pkl", 'wb'))
-pickle.dump(test_array, open("C:/Users/Joe/Downloads/fake-news/test_array.pkl", 'wb'))
-pickle.dump(labels, open("C:/Users/Joe/Downloads/fake-news/train_labels.pkl", 'wb'))
+pickle.dump(train_array, open("C:/Users/Joe/PycharmProjects/fakenews/data/train_array.pkl", 'wb'))
+pickle.dump(labels, open("C:/Users/Joe/PycharmProjects/fakenews/data/train_labels.pkl", 'wb'))
--- tree.py
+++ tree.py
@@ -1,22 +1,19 @@
 import pickle
-import csv
 from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 ###change these paths to be specific to your computer###
-train = pickle.load(open("C:/Users/Joe/Downloads/fake-news/train_array.pkl", 'rb'))
-labels = pickle.load(open("C:/Users/Joe/Downloads/fake-news/train_labels.pkl", 'rb'))
-test = pickle.load(open("C:/Users/Joe/Downloads/fake-news/test_array.pkl", 'rb'))
+train = pickle.load(open("C:/Users/Joe/PycharmProjects/fakenews/data/train_array.pkl", 'rb'))
+labels = pickle.load(open("C:/Users/Joe/PycharmProjects/fakenews/data/train_labels.pkl", 'rb'))
+
+data_train, data_test, labels_train, labels_test = train_test_split(train, labels, test_size = 0.33)
 
 model = tree.DecisionTreeClassifier()
-model.fit(train, labels) #training our decision tree on our labelled training data. It compares what it got (real or fake) to what the true answer is for each article, and adjusts itself accordingly
-predicted_labels = model.predict(test) #using our trained decision tree to predict answers (real or fake) for our test data
+model.fit(data_train, labels_train) #training our decision tree on our labelled training data. It compares what it got (real or fake) to what the true answer is for each article, and adjusts itself accordingly
+predicted_labels = model.predict(data_test) #using our trained decision tree to predict answers (real or fake) for our test data
 
-with open("C:/Users/Joe/Downloads/fake-news/labels.csv", 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file, delimiter=',')
-    writer.writerow(['Label'])
-    for n in range(len(predicted_labels)):
-        writer.writerow([predicted_labels[n]])
-
+print(accuracy_score(labels_test, predicted_labels))
 
 #Though it's clunky right now, you can compare what the decision tree classified the articles as,
 #based on amount of all-caps words and amount of curse words per article, to what you think each one
